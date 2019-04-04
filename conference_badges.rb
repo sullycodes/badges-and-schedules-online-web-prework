3ee956807e6e9ba0e701dc327754439d326375a5
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-04 00:40:19 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(array)
+  array.each do |speaker| puts "Hello, my name is #{speaker}"
+end
+
+def assign_rooms(array)
+  array.each do |speaker| puts "Hello #{speaker}! You'll be assigned to room #{speaker.each_with_index}."
+end 
